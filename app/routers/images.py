a7f0bd71baf6756232e1090782c4e593dc4a05f6
Nicolás Narváez <nicolas.narvaez06@outlook.com>
2022-12-11 18:20:16 -0500

    Add method to extract image features using VGG16

--- app/routers/images.py
+++ app/routers/images.py
@@ -1,7 +1,43 @@
 from fastapi import APIRouter, File, UploadFile
 
+import shutil
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Callable
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+model = VGG16(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation="softmax",
+)
+
 router = APIRouter()
+
+def extractFeatures(path):
+    img = image.load_img(path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    features = model.predict(x)
+    print (features)
 
 @router.post("/single-inference")
 async def singleInference(file: UploadFile):
-    return {"filename": file.filename}
+    try:
+        suffix = Path(file.filename).suffix
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(file.file, tmp)
+            tmp_path = Path(tmp.name)
+    finally:
+        file.file.close()
+    extractFeatures(tmp_path)
+    return tmp_path
